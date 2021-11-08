83d10ab68a1a7b2d5b5542c1da2caab174ac690e
Gediminas <bugedas@gmail.com>
2021-11-08 17:13:20 +0200

    Add users api requests

--- src/main/java/com/example/springsocial/controller/UserController.java
+++ src/main/java/com/example/springsocial/controller/UserController.java
@@ -1,5 +1,8 @@
 package com.example.springsocial.controller;
 
+import com.example.springsocial.api.exceptionHandling.exceptions.CustomDataNotFoundException;
+import com.example.springsocial.api.films.Film;
+import com.example.springsocial.api.posts.Post;
 import com.example.springsocial.exception.ResourceNotFoundException;
 import com.example.springsocial.model.User;
 import com.example.springsocial.repository.UserRepository;
@@ -8,7 +11,11 @@ import com.example.springsocial.security.UserPrincipal;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class UserController {
@@ -21,5 +28,14 @@ public class UserController {
     public User getCurrentUser(@CurrentUser UserPrincipal userPrincipal) {
         return userRepository.findById(userPrincipal.getId())
                 .orElseThrow(() -> new ResourceNotFoundException("User", "id", userPrincipal.getId()));
+    }
+
+    @GetMapping("api/user/id/{id}")
+    public User getUser(@PathVariable Long id) {
+        return userRepository.findById(id).orElseThrow(() -> new CustomDataNotFoundException("User of ID = " + id + " does not exist"));
+    }
+    @GetMapping("api/user")
+    public List<User> getUsers() {
+        return userRepository.findAll();
     }
 }
