3c68aec5d6514b9db65b241626dba7066e4b1268
Gediminas <bugedas@gmail.com>
2021-12-12 19:11:26 +0200

    Add user delete mappings

--- src/main/java/com/example/springsocial/controller/UserController.java
+++ src/main/java/com/example/springsocial/controller/UserController.java
@@ -9,7 +9,9 @@ import com.example.springsocial.repository.UserRepository;
 import com.example.springsocial.security.CurrentUser;
 import com.example.springsocial.security.UserPrincipal;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -37,5 +39,15 @@ public class UserController {
     @GetMapping("api/user")
     public List<User> getUsers() {
         return userRepository.findAll();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity deleteUser(@PathVariable Long id) {
+        try {
+            userRepository.deleteById(id);
+        } catch (Exception e) {
+            throw new CustomDataNotFoundException("User of ID = " + id + " does not exist");
+        }
+        return ResponseEntity.ok().build();
     }
 }
