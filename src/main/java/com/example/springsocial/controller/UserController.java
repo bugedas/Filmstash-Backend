044a6869291f3898968708b2f5527a7c71fc8a47
Gediminas <bugedas@gmail.com>
2022-04-15 01:48:03 +0300

    Last update before tests

--- src/main/java/com/example/springsocial/controller/UserController.java
+++ src/main/java/com/example/springsocial/controller/UserController.java
@@ -1,6 +1,14 @@
 package com.example.springsocial.controller;
 
+import com.example.springsocial.api.comments.Comment;
+import com.example.springsocial.api.comments.CommentRepository;
 import com.example.springsocial.api.exceptionHandling.exceptions.CustomDataNotFoundException;
+import com.example.springsocial.api.friends.Friend;
+import com.example.springsocial.api.friends.FriendRepository;
+import com.example.springsocial.api.posts.Post;
+import com.example.springsocial.api.posts.PostRepository;
+import com.example.springsocial.api.watchingNow.WatchingNow;
+import com.example.springsocial.api.watchingNow.WatchingNowRepository;
 import com.example.springsocial.exception.ResourceNotFoundException;
 import com.example.springsocial.model.User;
 import com.example.springsocial.repository.UserRepository;
@@ -16,6 +24,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class UserController {
@@ -24,7 +33,16 @@ public class UserController {
     private UserRepository userRepository;
 
     @Autowired
-    private PasswordEncoder passwordEncoder;
+    private FriendRepository friendRepository;
+
+    @Autowired
+    private PostRepository postsRepository;
+
+    @Autowired
+    private CommentRepository commentsRepository;
+
+    @Autowired
+    private WatchingNowRepository watchingNowRepository;
 
     @GetMapping("/api/user/me")
     @PreAuthorize("hasRole('USER')")
@@ -44,6 +62,7 @@ public class UserController {
         User currentUser = userRepository.findById(userPrincipal.getId())
                 .orElseThrow(() -> new ResourceNotFoundException("User", "id", userPrincipal.getId()));
         currentUser.setName(user.getName());
+        currentUser.setImageUrl(user.getImageUrl());
         currentUser.setUserPrivate(user.getUserPrivate());
         final User updatedUser = userRepository.save(currentUser);
 
@@ -76,5 +95,128 @@ public class UserController {
             throw new CustomDataNotFoundException("User of ID = " + id + " does not exist");
         }
         return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("api/user/metrics/{id}")
+    public UserMetrics getUserMetrics(@PathVariable Long id) {
+
+        List<Friend> allFriends = friendRepository.findAll();
+        int myFollowing = 0;
+        int myFollowers = 0;
+        for (Friend friend : allFriends) {
+            if(friend.getFollowingId().equals(id)){
+                myFollowing = myFollowing + 1;
+            }
+            if(friend.getFollowedId().equals(id)){
+                myFollowers = myFollowers + 1;
+            }
+        }
+
+        List<WatchingNow> allWatchingNow = watchingNowRepository.findAll();
+        int myWatching = 0;
+        int myWatched = 0;
+        for (WatchingNow watching : allWatchingNow) {
+            if(watching.getUserId().equals(id) && watching.getStillWatching()){
+                myWatching = myWatching + 1;
+            }
+            if(watching.getUserId().equals(id) && watching.getFinished()){
+                myWatched = myWatched + 1;
+            }
+        }
+
+        List<Post> allPosts = postsRepository.findAll();
+        int myPostsCount = 0;
+        int myPostsLikeCount = 0;
+        for (Post post : allPosts) {
+            if(post.getUserId().equals(id)){
+                myPostsCount = myPostsCount + 1;
+                myPostsLikeCount = myPostsLikeCount + post.getLikes();
+            }
+        }
+
+        List<Comment> allComments = commentsRepository.findAll();
+        int myCommentsCount = 0;
+        for (Comment comment : allComments) {
+            if(comment.getUserId().equals(id)){
+                myCommentsCount = myCommentsCount + 1;
+            }
+        }
+
+        UserMetrics userMetrics = new UserMetrics();
+        userMetrics.setFollowingCount(myFollowing);
+        userMetrics.setFollowersCount(myFollowers);
+        userMetrics.setWatchingTvSeriesCount(myWatching);
+        userMetrics.setWatchedTvSeriesCount(myWatched);
+        userMetrics.setMyPostsCount(myPostsCount);
+        userMetrics.setMyPostsLikeCount(myPostsLikeCount);
+        userMetrics.setMyCommentsCount(myCommentsCount);
+
+        return userMetrics;
+    }
+}
+
+class UserMetrics {
+    int followingCount;
+    int followersCount;
+    int watchingTvSeriesCount;
+    int watchedTvSeriesCount;
+    int myPostsCount;
+    int myPostsLikeCount;
+    int myCommentsCount;
+
+    public int getFollowingCount() {
+        return followingCount;
+    }
+
+    public void setFollowingCount(int followingCount) {
+        this.followingCount = followingCount;
+    }
+
+    public int getFollowersCount() {
+        return followersCount;
+    }
+
+    public void setFollowersCount(int followersCount) {
+        this.followersCount = followersCount;
+    }
+
+    public int getWatchingTvSeriesCount() {
+        return watchingTvSeriesCount;
+    }
+
+    public void setWatchingTvSeriesCount(int watchingTvSeriesCount) {
+        this.watchingTvSeriesCount = watchingTvSeriesCount;
+    }
+
+    public int getWatchedTvSeriesCount() {
+        return watchedTvSeriesCount;
+    }
+
+    public void setWatchedTvSeriesCount(int watchedTvSeriesCount) {
+        this.watchedTvSeriesCount = watchedTvSeriesCount;
+    }
+
+    public int getMyPostsCount() {
+        return myPostsCount;
+    }
+
+    public void setMyPostsCount(int myPostsCount) {
+        this.myPostsCount = myPostsCount;
+    }
+
+    public int getMyCommentsCount() {
+        return myCommentsCount;
+    }
+
+    public void setMyCommentsCount(int myCommentsCount) {
+        this.myCommentsCount = myCommentsCount;
+    }
+
+    public int getMyPostsLikeCount() {
+        return myPostsLikeCount;
+    }
+
+    public void setMyPostsLikeCount(int myPostsLikeCount) {
+        this.myPostsLikeCount = myPostsLikeCount;
     }
 }
